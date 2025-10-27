687dbc7fdf65716e339d9ad1c979d70d478e3f20
Hoang <nvhoangag@gmail.com>
2025-10-27 22:01:39 +0700

    WP: update page header

--- wordpress/wp-content/themes/raymag/functions.php
+++ wordpress/wp-content/themes/raymag/functions.php
@@ -10,7 +10,7 @@
 
 if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '1.0.1');
+	define('_S_VERSION', '1.0.2');
 }
 
 if (! defined('_LANG_DOMAIN')) {
