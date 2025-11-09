3de27835a6d7da91b5a39e248d66fc1e2861cc0a
Hoang <nvhoangag@gmail.com>
2025-11-09 17:19:05 +0700

    no message

---
+++ wordpress/wp-content/themes/raymag/raymag_template_contact.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Template Name: Raymag Template - Contact
+ * Template Post Type: page
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package raymag
+ */
+
+get_header();
+?>
+
+<?php
+get_footer();
